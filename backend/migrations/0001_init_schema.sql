fc0003028a4a292383945155a126b8016b2eef13
jade <jade@hopepunk.me>
2025-01-25 11:34:48 +1030

    removed regions table

--- backend/migrations/0001_init_schema.sql
+++ backend/migrations/0001_init_schema.sql
@@ -1,14 +1,7 @@
 -- Add migration script here
-CREATE TABLE regions (
-    id VARCHAR(36) PRIMARY KEY NOT NULL,
-    name VARCHAR(255) NOT NULL
-);
-
 CREATE TABLE sites (
     id VARCHAR(36) PRIMARY KEY NOT NULL,
-    region_id VARCHAR(36) ,
-    name VARCHAR(50) NOT NULL,
-    FOREIGN KEY (region_id) REFERENCES regions(id)
+    name VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE network_configs (
@@ -21,7 +14,6 @@ CREATE TABLE network_configs (
 CREATE TABLE site_configs (
     id INT PRIMARY KEY NOT NULL,
     this_site_id VARCHAR(36),
-    this_region_id VARCHAR(36),
     private_key_hex VARCHAR(64),
     FOREIGN KEY (this_site_id) REFERENCES sites(id)
 );
